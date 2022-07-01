b85ba6b99a3c29c7499cd56a2c7d522a54ca0e7f
fherrerao <fherrera0206@gmail.com>
2022-07-01 09:17:44 -0500

    Add routes for the API

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api, :defaults => { :format => :json } do
+    namespace :v1 do
+      resources :projects
+    end
+  end
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "projects#index"
 end
