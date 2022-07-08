4024ab76a75e3133ac17cc8b8e22fab20b0a0cca
fherrerao <fherrera0206@gmail.com>
2022-07-08 12:26:52 -0500

    Add validations for a project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,8 @@
 class Project < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :tech_1, presence: true
+  validates :tech_2, presence: true  
 end
