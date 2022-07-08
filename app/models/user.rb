efa05ee2cff19eccc48f5782851e80456c327437
fherrerao <fherrera0206@gmail.com>
2022-07-08 12:27:12 -0500

    Add admin method in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :projects, dependent: :delete_all
+
+  def admin?
+    role == "admin"
+  end
 end
